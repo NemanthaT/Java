e842ee977b9df46df159939701148a5a598e725d
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-06-07 22:08:39 +0530

    Refactor CarDete class to improve code structure and user experience (P.S.-Still not working)

--- CarDete.java
+++ CarDete.java
@@ -3,10 +3,22 @@ import java.util.Stack;
 
 public class CarDete{
 
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
+    static Stack<Car> carStack = new Stack<>();
+    static boolean cond=true;
+    static Scanner sc= new Scanner(System.in);
+    public static boolean choice(char ch){
+        //Scanner sc= new Scanner(System.in);
+        if(ch=='y'){
+            enterDete();
+        }
+        else{
+            cond=false;
+        }
+        return cond;
+    }
 
-
+    public static void enterDete(){
+        Scanner scan= new Scanner(System.in);
         String nam;
         String modl;
         String clr;
@@ -14,15 +26,50 @@ public class CarDete{
 
         System.out.print("Enter Car name: ");
         nam=sc.nextLine();
+
         System.out.print("Enter Car model: ");
         modl=sc.nextLine();
+
         System.out.print("Enter Car color: ");
         clr=sc.nextLine();
+
         System.out.print("Enter Car year: ");
         yr=sc.nextInt();
+
         Car cars= new Car(nam,modl,clr,yr);
+        carStack.push(cars);
+        String cho;
+        System.out.print("Do you want to add more cars (y/n): ");
+        cho=scan.nextLine();
+        char ch=cho.charAt(0);
+        choice(ch);
+    }
+
+    public static void printCarStack(Stack<Car> carStack){
+        for (Car car : carStack){
+            car.displayInfo();
+            System.out.println("-----------------------------------");
+        }
+    }
+
+    public static void main(String[] args){
+        while(cond==true){
+            //Scanner scan= new Scanner(System.in);
+            enterDete();
+            /*String cho;
+            System.out.println("Do you want to add more cars (y/n): ");
+            cho=scan.nextLine();
+            char ch=cho.charAt(0);
+            if(ch=='y'){
+                enterDete();
+            }
+            else{
+                cond=false;
+            }*/
+        }
+
         System.out.println("-----------------------------------");
         System.out.println("Car details,");
-        cars.displayInfo();
+        printCarStack(carStack);
     }
 }
