eda17b0d79a63916ef83f775b7858ae038483efc
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-06-07 22:45:01 +0530

    feat: Add refno parameter to Car constructor

--- Car.java
+++ Car.java
@@ -3,12 +3,14 @@ public class Car{
     String model;
     String color;
     int year;
+    int refno;
 
-    public Car(String name, String model, String color, int year){
+    public Car(String name, String model, String color, int year, refno){
         this.name=name;
         this.model=model;
         this.color=color;
         this.year=year;
+        this.refno=refno;
     }
 
     public void displayInfo(){
