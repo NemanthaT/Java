530bf294e161666f9be1924f75b05f5ce771354c
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-06-15 23:19:13 +0530

    Refactor p2q5.java to add a do-while loop for printing numbers from 10 to 1

---
+++ p2q5.java
@@ -0,0 +1,18 @@
+public class p2q5{
+    public static void main(String[] args){
+        int i=10;
+
+        while(i>0){
+            System.out.println(i);
+            i--;
+        }
+        System.out.println("-----------------------");
+        i=10;
+        
+        do{
+            System.out.println(i);
+            i--;
+        }
+        while(i>0);
+    }
+}
