de45da6c8ca021ce1c06d29999327cb55b02451d
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-06-15 22:48:46 +0530

    Refactor p2q1.java to perform basic arithmetic operations on two numbers

---
+++ p2q1.java
@@ -0,0 +1,19 @@
+public class p2q1 {
+    public static void main (String[] args){
+        int a=30;
+        int b=10;
+        int c=3;
+
+        int add=a+b;
+        int subs=a-b;
+        int multi=a*b;
+        int divi=a/b;
+        int remain=a%c;
+
+        System.out.println("Addition: "+add);
+        System.out.println("Subtraction: "+subs);
+        System.out.println("Multiplication: "+multi);
+        System.out.println("Division: "+divi);
+        System.out.println("Remainder: "+remain);
+    }
+}
