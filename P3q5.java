cf657ffe5ec24792d1d12cff81026fec37115442
Tharusha Nemantha <nemanathatharusha@gmail.com>
2024-06-21 16:15:10 +0530

    Refactor P3q5.java to remove duplicate characters from a sentence

--- P3q5.java
+++ P3q5.java
@@ -7,40 +7,84 @@ public class P3q5{
 		System.out.print("Enter a sentence: ");
 		String mySent = scn.nextLine();
 		String dupSent = String.valueOf(mySent);
+		Stack<Character> checkSent = new Stack<>();
 		Stack<Character> newSent = new Stack<>();
+		Stack<Character> finalSent = new Stack<>();
 		int i=0;
 		int r=0;
 		char c=' ';
-		newSent.push(mySent.charAt(0));
-		
+
 		for(i=0;i<mySent.length();i++){
+			newSent.push(mySent.charAt(i));
+		}
+		for(i=0;i<mySent.length();i++){
+			checkSent.push(mySent.charAt(i));
+		}
+		
+		for(char k: checkSent){
+			for(char j: newSent){
+				if (k==j){
+					//System.out.println("k: "+k+" j: "+j);
+					continue;						
+				}
+				else{
+					c=k;
+				}
+			}
+			finalSent.push(c);
+			
+		}
+		/*for(i=0;i<mySent.length();i++){
 			for(char j: newSent){
 				if (mySent.charAt(i)!=j){
 					System.out.println("i: "+mySent.charAt(i)+" j: "+j);
-					c=mySent.charAt(i);
-					System.out.println("c: "+ mySent.charAt(i));			
+					c = mySent.charAt(i);
+					System.out.println("c: "+c);
+						
+					//String ch = c;				
 				}
-				else{
-					continue;
-				}
-				newSent.push(c);
-				
 			}
-				
-			
-		}
+			newSent.push(c);	
+		}*/
 		System.out.print("Modified: ");
-		i=0;
 		/*while(!(newSent.isEmpty())){
 			System.out.print(newSent[i]);
 			i++;
 		}*/
-		for(char y : newSent){
-			System.out.print(y);
+		while(!(finalSent.isEmpty())){
+			System.out.print(finalSent.pop());
 		}
-		System.out.print("\n");
+
+		System.out.println("\n");
 		System.out.println(mySent);
 		System.out.println(dupSent);
 		System.out.println(dupSent.length());
 	}
 }
+
+/* 
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+        
+        String result = removeDuplicates(input);
+        System.out.println("String after removing duplicates: " + result);
+    }
+
+    public static String removeDuplicates(String input) {
+        boolean[] seen = new boolean[256]; // Array to track seen characters (assuming ASCII)
+        char[] resultArray = new char[input.length()];
+        int index = 0;
+
+        for (char c : input.toCharArray()) {
+            if (!seen[c]) {
+                seen[c] = true;
+                resultArray[index++] = c;
+            }
+        }
+
+        return new String(resultArray, 0, index);
+    }
+}*/
